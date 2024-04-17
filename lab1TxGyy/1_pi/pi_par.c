4aa2b8820641dffe6fff65cb4195524e5951f138
aniolpetit <aniol.petit01@estudiant.upf.edu>
2024-04-17 17:32:29 +0200

    pi_par correct

--- lab1TxGyy/1_pi/pi_par.c
+++ lab1TxGyy/1_pi/pi_par.c
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 #include <omp.h>
 
+//IF N IS GREAT (1024^3 FOR INSTANCE) I DON'T GET A CORRECT RESULT, ACTUALLY NOTHING. WHY?
 int main(int argc, char* argv[]) {
     if(argc < 2) {
         printf("N must be passed as a parameter!");
@@ -9,22 +10,36 @@ int main(int argc, char* argv[]) {
     }
 
     int N = atoi(argv[1]);
+    omp_set_num_threads(4);
     float pi = 0;
     float width = (float) 1/N;
     float xi = 0;
     float start_time = omp_get_wtime();
+    int nthreads = omp_get_num_threads();
+    float *local_sum = (float *)malloc(nthreads * sizeof(float));
 
     // Parallel region starts here
     #pragma omp parallel
     {
-        float local_sum = 0.0f;
-        for(int i = 0; i < N; i++) {
-            xi = (i + 0.5) * width;
-            local_sum += 4 / (1 + xi * xi);
+        int id = omp_get_thread_num();
+       
+        int first = N/nthreads*id;
+        int last = N/nthreads*(id+1);
+
+        if(last > N){
+            last = N;
         }
-        pi += local_sum;
+        
+        local_sum[id] = 0.0f;
+        for(int i = first; i < last; i++) {
+            xi = (i + 0.5) * width;
+            local_sum[id] += 4 / (1 + xi * xi);
+        }
     }
     // Parallel region ends here
+    for(int i = 0; i < nthreads; i++) {
+        pi += local_sum[i];
+    }
 
     pi *= width;
     float end_time = omp_get_wtime();
