83b845d6f2f4558adf853f5cdb1513e7fab98f0a
aniolpetit <aniol.petit01@estudiant.upf.edu>
2024-04-18 15:05:19 +0200

    pi finished

--- lab1TxGyy/1_pi/pi_task.c
+++ lab1TxGyy/1_pi/pi_task.c
@@ -2,41 +2,58 @@
 #include <stdlib.h>
 #include <omp.h>
 
-int main(int argc, char* argv[]) {
+
+float compute_pi(int start, int end, float width) {
+    float partial_pi = 0;
+    float xi, fxi;
+    for (int i = start; i < end; i++) {
+        xi = (i + 0.5) * width;
+        fxi = 4 / (1 + xi * xi);
+        partial_pi += fxi;
+    }
+    return partial_pi;
+}
+
+void compute_pi_task(int start, int end, float width, float *result, int M) {
+    if (end - start <= M) {
+        *result = compute_pi(start, end, width);
+    } 
+    else {
+        int mid = (start + end) / 2;
+        float partial_pi1, partial_pi2;
+        #pragma omp task shared(partial_pi1) firstprivate(start,mid,width,M)
+                compute_pi_task(start, mid, width, &partial_pi1, M);
+        #pragma omp task shared(partial_pi2) firstprivate(mid,end,width,M)
+                compute_pi_task(mid, end, width, &partial_pi2, M);
+        #pragma omp taskwait
+        *result = partial_pi1 + partial_pi2;
+    }
+}
+
+int main(int argc, char *argv[]) {
     if (argc < 3) {
         printf("N and M must be passed as parameters!");
-        exit(0);
+        exit(1);
     }
-
+    
     int N = atoi(argv[1]);
-    int M = atoi(argv[2]); // Minimum number of steps in a task
-    float pi = 0;
-    float width = 1.0f / N;
-    float start_time = omp_get_wtime();
-
-    #pragma omp parallel
-    #pragma omp single
-    {
-        int num_tasks = (N + M - 1) / M; // Calculate number of tasks needed
-
-        // Create tasks for each chunk of work
-        for (int i = 0; i < num_tasks; i++) {
-            int start = i * M;
-            int end = (i == num_tasks - 1) ? N : start + M;
-
-            #pragma omp task shared(pi)
-            {
-                float partial_pi = 0.0f;
-                for (int j = start; j < end; j++) {
-                    float xi = (j + 0.5) * width;
-                    partial_pi += 4 / (1 + xi * xi);
-                }
-                #pragma omp atomic
-                pi += partial_pi;
-            }
-        }
+    int M = atoi(argv[2]);
+    
+    if (M < 1024) {
+        printf("Each task must perform at least  1024 steps (M>=1024) ! \n");
+        exit(1);
     }
 
+    float pi = 0;
+    float width = 1.0 / N;
+    float start_time = omp_get_wtime();
+    #pragma omp parallel
+    {
+        #pragma omp single
+        {
+            compute_pi_task(0, N, width, &pi, M);
+        }        
+    }
     pi *= width;
     float end_time = omp_get_wtime();
     float runtime = end_time - start_time;
